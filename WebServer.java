083f25448ff33ab15ee6ee44f22d3243c082c109
Jens Albert <github@jomagus.de>
2015-11-03 13:03:47 +0100

    started implementing the Server

---
+++ WebServer.java
@@ -0,0 +1,32 @@
+import java.io.* ;
+import java.net.* ;
+import java.util.* ;
+
+/**
+ *
+ */
+public final class WebServer
+{
+    public static void main(String argv[]) throws Exception
+    {
+        final int PORT = 6789;                                          // auf diesem Port wird unser Webserver laufen.
+
+        //TODO Establish the listen socket.
+
+        //TODO Process HTTP service requests in an infinite loop.
+        while (true) {
+            //TODO Listen for a TCP connection request.
+
+
+        }
+
+    }
+}
+
+/**
+ *
+ */
+final class HttpRequest implements Runnable
+{
+
+}
