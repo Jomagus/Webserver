f78f04451db841a5ba032af007586ea3cce8651f
Jomagus <github@jomagus.de>
2015-11-04 14:57:54 +0100

    opened the Server Socket

--- WebServer.java
+++ WebServer.java
@@ -11,11 +11,31 @@ public final class WebServer
     {
         final int PORT = 6789;                                          // auf diesem Port wird unser Webserver laufen.
 
-        //TODO Establish the listen socket.
+        // Wir öffnen hier einen neuen Serversocket der auf eingehende Verbindungen wartet
+
+        ServerSocket MeinTollerServerSocket = null;
+        try {
+            MeinTollerServerSocket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            System.err.println("IO-Fehler beim öffnen des Sockets aufgetreten. Breche ab...");
+            System.exit(-1);
+        } catch (SecurityException e) {
+            System.err.println("Nicht genug Rechte zum öffnen des Sockets. Breche ab...");
+            System.exit(-1);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Gewünschter Port nicht belegbar. Breche ab...");
+            System.exit(-1);
+        } finally {
+            if (MeinTollerServerSocket == null) {
+                System.err.println("Unbekannter Fehler beim öffnen des Sockets aufgetreten. Breche ab...");
+                System.exit(-1);
+            }
+        }
 
         //TODO Process HTTP service requests in an infinite loop.
         while (true) {
             //TODO Listen for a TCP connection request.
+            Socket request = MeinTollerServerSocket.accept();
 
 
         }
